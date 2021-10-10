6ac651ba0a3f73c7677567b25fe7ecf2454b2466
SharzyL <me@sharzy.in>
2021-10-10 16:49:25 +0800

    style: educated by clang-tidy

--- Thread-1/q0.cc
+++ Thread-1/q0.cc
@@ -15,9 +15,10 @@ namespace proj1 {
 void run_one_instruction(Instruction inst, EmbeddingHolder* users, EmbeddingHolder* items) {
     switch(inst.order) {
         case INIT_EMB: {
+            // read items[item_idx...], write a new user
             // We need to init the embedding
             int length = users->get_emb_length();
-            Embedding* new_user = new Embedding(length);
+            auto* new_user = new Embedding(length);
             int user_idx = users->append(new_user);
             for (int item_index: inst.payloads) {
                 Embedding* item_emb = items->get_embedding(item_index);
@@ -28,6 +29,7 @@ void run_one_instruction(Instruction inst, EmbeddingHolder* users, EmbeddingHold
             break;
         }
         case UPDATE_EMB: {
+            // read users[user_idx], items[item_idx], update users[user_idx]
             int user_idx = inst.payloads[0];
             int item_idx = inst.payloads[1];
             int label = inst.payloads[2];
@@ -44,6 +46,7 @@ void run_one_instruction(Instruction inst, EmbeddingHolder* users, EmbeddingHold
             break;
         }
         case RECOMMEND: {
+            // read users[user_idx], read items[item_idx...]
             int user_idx = inst.payloads[0];
             Embedding* user = users->get_embedding(user_idx);
             std::vector<Embedding*> item_pool;
@@ -61,14 +64,13 @@ void run_one_instruction(Instruction inst, EmbeddingHolder* users, EmbeddingHold
 } // namespace proj1
 
 int main(int argc, char *argv[]) {
-
-    proj1::EmbeddingHolder* users = new proj1::EmbeddingHolder("data/q0.in");
-    proj1::EmbeddingHolder* items = new proj1::EmbeddingHolder("data/q0.in");
+    auto* users = new proj1::EmbeddingHolder("data/q0.in");
+    auto* items = new proj1::EmbeddingHolder("data/q0.in");
     proj1::Instructions instructions = proj1::read_instructrions("data/q0_instruction.tsv");
     {
     proj1::AutoTimer timer("q0");  // using this to print out timing of the block
     // Run all the instructions
-    for (proj1::Instruction inst: instructions) {
+    for (const proj1::Instruction& inst: instructions) {
         proj1::run_one_instruction(inst, users, items);
     }
     }
